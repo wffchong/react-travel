519fdc7de7819e6e98b6a14d5b78e99a75ff99f4
wffchong <283953455@qq.com>
2022-09-03 23:26:27 +0800

    使用redux-persist完成状态持久化

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App'
 import './i18n'
 import { Provider } from 'react-redux'
-import store from './store'
+import { store, persistor } from './store'
+//如果使用React,则使用 PersistGate 包裹根组建
+import { PersistGate } from 'redux-persist/lib/integration/react'
+
 import axios from 'axios'
 axios.defaults.headers['x-icode'] = '887E07F53CDA6854'
 axios.defaults.baseURL = 'http://123.56.149.216:8080'
@@ -13,6 +16,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
     <Provider store={store}>
-        <App />
+        {/* // loading 和 persistor是2个必需属性 */}
+        {/* // loading={null} || loading={<LoadingView />} LoadingView为React组件 */}
+        {/* // 最好将loading={null}，写成loading={<LoadingView />} 报错，原因暂不明 */}
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,24 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import languageReducer from './modules/language'
 import recommendProductsReducer from './modules/recommendProducts'
 import productDetailReducer from './modules/productDetail'
 import productSearchReducer from './modules/productSearch'
 import userReducer from './modules/user'
+
+//配置数据的持久化效果
+import { persistStore, persistReducer } from 'redux-persist'
+//导入需要配置的数据源，可以选择，storage，cookie,session等
+import storage from 'redux-persist/lib/storage'
+
+//定义配置的信息
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    // 如果不想将部分state持久化，可以将其放入黑名单(blacklist)中.黑名单是设置
+    // blacklist: ['']
+    // 白名单 --> 和黑名单只需要使用一个就行
+    whitelist: ['user']
+}
 
 const rootReducer = combineReducers({
     language: languageReducer,
@@ -13,11 +28,22 @@ const rootReducer = combineReducers({
     user: userReducer
 })
 
+//创建持久化的配置persist的信息
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    // 解决控制台报错无法序列号
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false
+        }),
     devTools: true
 })
 
+//使用persistStore包裹一下
+const persistor = persistStore(store)
+
 export type RootStateType = ReturnType<typeof store.getState>
 
-export default store
+export { store, persistor }
